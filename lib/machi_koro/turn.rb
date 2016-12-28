797f17b6abcbfba1f97793fed6d42232d695573c
reedstonefood <reedstonefood@users.noreply.github.com>
2016-12-28 17:25:24 +0000

    Split BuildingResolver out of Turn
    Trying to improve CodeBeat score

--- lib/machi_koro/turn.rb
+++ lib/machi_koro/turn.rb
@@ -8,7 +8,6 @@ module MachiKoro
       @dice_count = dice_count
       @reroll_available = reroll_available
       @harbour_available = harbour_available
-      @harbour_used = false
       @log = log
     end
     
@@ -21,10 +20,7 @@ module MachiKoro
         @reroll_available = false
         @log.add(__callee__, "Re-roll selected")
       end
-      @dice = Array.new()
-      @dice_count.times { @dice << rand(6) }
-      @log.add(__callee__, "The dice were rolled : #{@dice}")
-      @dice
+      do_roll
     end
     
     def sum_dice(input = @roll)
@@ -32,14 +28,13 @@ module MachiKoro
     end
     
     def use_harbour
-      if @harbour_used==false && @sum_dice >=10 && @harbour_available == true
+      if @sum_dice >=10 && @harbour_available == true
         @sum_dice += 2
-        @harbour_used = true
+        @harbour_available = false
         @log.add(__callee__, 'Harbour has been activated')
         true
-      else
-        @log.add(__callee__, 'Harbour is not useable')
-        false
+      else # harbour has been used - or player does not have a harbour
+        @log.add(__callee__, 'Harbour is not useable'); false
       end
     end
     
@@ -49,8 +44,91 @@ module MachiKoro
       end
       return false
     end
+    
+    private
+    def do_roll
+      @dice = Array.new()
+      @dice_count.times { @dice << rand(6) }
+      @log.add(__callee__, "The dice were rolled : #{@dice}")
+      @dice
+    end
+    
   end
 
+  class BuildingResolver
+  
+    attr_accessor :roll_val
+  
+    def initialize(log, player, players)
+      @log = log
+      @p = player
+      @players = players
+    end
+  
+    # anti-clockwise, starting with the player whose turn it is
+    def resolution_order
+      return @resolution_order if defined? @resolution_order
+      @resolution_order = @players.reverse
+      @resolution_order.rotate!(@resolution_order.index(@p))
+    end
+    
+    # returns a hash of 4 values, one for each colour.
+    # each hash is an array of "slots"
+    # a slot is an array of the format [card, card_count, player]
+    def get_cards(roll_val = @roll_val)
+      active_slots = Hash[:blue => Array.new(), :green => Array.new(), :red => Array.new(), :purple => Array.new()]
+      resolution_order.each do |player|
+        @p == player ? curr_player = true : curr_player = false
+        player.tableau.deck.each do |slot|
+          if slot[0].is_activated(roll_val, curr_player)
+            active_slots[slot[0].attribute[:colour]] << (slot << player) 
+          end
+        end
+      end
+      return active_slots
+    end
+  
+    def process_cards(roll_val = @roll_val)
+      slots = get_cards(roll_val)
+      process_blue(slots[:blue])
+      process_green(slots[:green])
+      process_red(slots[:red])
+      process_purple(slots[:purple])
+    end
+    
+    def process_blue(blue_array)
+      #curr_player = 0
+      blue_array.each do |slot|
+        income = slot[0].attribute[:base_income] * slot[1]
+        @log.add(__callee__, "#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income}")
+        slot[2].money += income
+      end
+    end
+    
+    def process_green(green_array)
+      green_array.each do |slot|
+        income = slot[0].attribute[:base_income] * slot[1]
+        @log.add(__callee__,"#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income}")
+        slot[2].money += income
+      end
+    end
+    
+    def process_red(red_array)
+      red_array.each do |slot|
+        income = slot[0].attribute[:base_income] * slot[1]
+        income = @p.money if @p.money < income
+        @log.add(__callee__,"#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income} from #{@p.name}")
+        slot[2].cash += income
+        @p.cash += -income
+      end
+    end
+    
+    def process_purple(purple_array)
+      #TODO once purple cards are added
+    end
+  
+  end
+  
   class Turn
 
     # each turn, the following steps happen / must be checked
@@ -73,8 +151,7 @@ module MachiKoro
       #@p_abilities = @p.built_landmarks.reduce([]) { |arr, l| arr.concat(l.ability) }
       @purchased_card = nil
       @stage = @@stages[0]
-      
-      
+      @BuildingResolver = BuildingResolver.new(game.log, player, game.players)
     end
     
     def roll_dice(dice_count)
@@ -117,67 +194,13 @@ module MachiKoro
       end
     end 
     
-    # anti-clockwise, starting with the player whose turn it is
-    def resolution_order
-      return @resolution_order if defined? @resolution_order
-      @resolution_order = @g.players.reverse
-      @resolution_order.rotate!(@resolution_order.index(@p))
-    end
-    
-    # returns a hash of 4 values, one for each colour.
-    # each hash is an array of "slots"
-    # a slot is an array of the format [card, card_count, player]
-    def get_cards(roll_val = @reroll)
-      active_slots = Hash[:blue => Array.new(), :green => Array.new(), :red => Array.new(), :purple => Array.new()]
-      resolution_order.each do |player|
-        @p == player ? curr_player = true : curr_player = false
-        player.tableau.deck.each do |slot|
-          if slot[0].is_activated(roll_val, curr_player)
-            active_slots[slot[0].attribute[:colour]] << (slot << player) 
-          end
-        end
-      end
-      return active_slots
-    end
-    
     def process_cards(roll_val)
-      slots = get_cards(roll_val)
-      process_blue(slots[:blue])
-      process_green(slots[:green])
-      process_red(slots[:red])
-      process_purple(slots[:purple])
+      @BuildingResolver.process_cards(roll_val)
     end
     
-    def process_blue(blue_array)
-      #curr_player = 0
-      blue_array.each do |slot|
-        income = slot[0].attribute[:base_income] * slot[1]
-        @g.log.add(__callee__, "#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income}")
-        slot[2].money += income
-      end
-    end
+    def resolution_order; @BuildingResolver.resolution_order; end
     
-    def process_green(green_array)
-      green_array.each do |slot|
-        income = slot[0].attribute[:base_income] * slot[1]
-        @g.log.add(__callee__,"#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income}")
-        slot[2].money += income
-      end
-    end
-    
-    def process_red(red_array)
-      red_array.each do |slot|
-        income = slot[0].attribute[:base_income] * slot[1]
-        income = @p.money if @p.money < income
-        @g.log.add(__callee__,"#{slot[2].name} has #{slot[1]} x #{slot[0].attribute[:name]} = #{income} from #{@p.name}")
-        slot[2].cash += income
-        @p.cash += -income
-      end
-    end
-    
-    def process_purple(purple_array)
-      #TODO once purple cards are added
-    end
+    def get_cards(roll_val); @BuildingResolver.get_cards(roll_val); end
     
   end
 
